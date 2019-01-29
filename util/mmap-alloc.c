d71481c4eab0911ba56b310aef97606fd0c07424
Murilo Opsfelder Araujo <muriloo@linux.ibm.com>
2019-01-29 17:20:15 -0200

    mmap-alloc: fix hugetlbfs misaligned length in ppc64
    The commit 7197fb4058bcb68986bae2bb2c04d6370f3e7218 ("util/mmap-alloc:
    fix hugetlb support on ppc64") fixed Huge TLB mappings on ppc64.
    
    However, we still need to consider the underlying huge page size
    during munmap() because it requires that both address and length be a
    multiple of the underlying huge page size for Huge TLB mappings.
    Quote from "Huge page (Huge TLB) mappings" paragraph under NOTES
    section of the munmap(2) manual:
    
      "For munmap(), addr and length must both be a multiple of the
      underlying huge page size."
    
    On ppc64, the munmap() in qemu_ram_munmap() does not work for Huge TLB
    mappings because the mapped segment can be aligned with the underlying
    huge page size, not aligned with the native system page size, as
    returned by getpagesize().
    
    This has the side effect of not releasing huge pages back to the pool
    after a hugetlbfs file-backed memory device is hot-unplugged.
    
    This patch fixes the situation in qemu_ram_mmap() and
    qemu_ram_munmap() by considering the underlying page size on ppc64.
    
    After this patch, memory hot-unplug releases huge pages back to the
    pool.
    
    Fixes: 7197fb4058bcb68986bae2bb2c04d6370f3e7218
    Signed-off-by: Murilo Opsfelder Araujo <muriloo@linux.ibm.com>

--- util/mmap-alloc.c
+++ util/mmap-alloc.c
@@ -80,6 +80,7 @@ void *qemu_ram_mmap(int fd, size_t size, size_t align, bool shared)
     int flags;
     int guardfd;
     size_t offset;
+    size_t pagesize;
     size_t total;
     void *ptr1;
     void *ptr;
@@ -100,7 +101,8 @@ void *qemu_ram_mmap(int fd, size_t size, size_t align, bool shared)
      * anonymous memory is OK.
      */
     flags = MAP_PRIVATE;
-    if (fd == -1 || qemu_fd_getpagesize(fd) == getpagesize()) {
+    pagesize = qemu_fd_getpagesize(fd);
+    if (fd == -1 || pagesize == getpagesize()) {
         guardfd = -1;
         flags |= MAP_ANONYMOUS;
     } else {
@@ -108,6 +110,7 @@ void *qemu_ram_mmap(int fd, size_t size, size_t align, bool shared)
     }
 #else
     guardfd = -1;
+    pagesize = getpagesize();
     flags = MAP_PRIVATE | MAP_ANONYMOUS;
 #endif
 
@@ -119,7 +122,7 @@ void *qemu_ram_mmap(int fd, size_t size, size_t align, bool shared)
 
     assert(is_power_of_2(align));
     /* Always align to host page size */
-    assert(align >= getpagesize());
+    assert(align >= pagesize);
 
     flags = MAP_FIXED;
     flags |= fd == -1 ? MAP_ANONYMOUS : 0;
@@ -142,17 +145,24 @@ void *qemu_ram_mmap(int fd, size_t size, size_t align, bool shared)
      * a guard page guarding against potential buffer overflows.
      */
     total -= offset;
-    if (total > size + getpagesize()) {
-        munmap(ptr1 + size + getpagesize(), total - size - getpagesize());
+    if (total > size + pagesize) {
+        munmap(ptr1 + size + pagesize, total - size - pagesize);
     }
 
     return ptr1;
 }
 
-void qemu_ram_munmap(void *ptr, size_t size)
+void qemu_ram_munmap(int fd, void *ptr, size_t size)
 {
+    size_t pagesize;
+
     if (ptr) {
         /* Unmap both the RAM block and the guard page */
-        munmap(ptr, size + getpagesize());
+#if defined(__powerpc64__) && defined(__linux__)
+        pagesize = qemu_fd_getpagesize(fd);
+#else
+        pagesize = getpagesize();
+#endif
+        munmap(ptr, size + pagesize);
     }
 }
